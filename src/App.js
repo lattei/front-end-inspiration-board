c936be9e75a0378da6fdbe904ff23550de38b16d
Carline <carlinesenat@gmail.com>
2023-07-20 10:44:08 -0700

    created components folder and moved header to header.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 import axios from "axios";
+import Header from "./components/header";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
@@ -219,49 +220,49 @@ function App() {
   );
 }
 
-function Header({ showForm, setShowForm, addBoard, handleFormToggle, handleNewBoardToggle, handleExistingBoardToggle }) {
-  const appTitle = "Everything I've Learned, I Got From...";
-  const [board, setBoard] = useState("");
+// function Header({ showForm, setShowForm, addBoard, handleFormToggle, handleNewBoardToggle, handleExistingBoardToggle }) {
+//   const appTitle = "Everything I've Learned, I Got From...";
+//   const [board, setBoard] = useState("");
 
-  const handleBoardSubmit = (event) => {
-    event.preventDefault();
+//   const handleBoardSubmit = (event) => {
+//     event.preventDefault();
 
-    if (board) {
-      const newBoard = { name: board, color: getRandomColor() };
-      addBoard(newBoard);
-      setBoard("");
-    }
-  };
+//     if (board) {
+//       const newBoard = { name: board, color: getRandomColor() };
+//       addBoard(newBoard);
+//       setBoard("");
+//     }
+//   };
 
-  return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Everything I've Learned logo" />
-        <h1>{appTitle}</h1>
-      </div>
-      <button className="btn btn-large btn-open" onClick={handleFormToggle}>
-        {showForm ? "Close" : "Share your inspiration"}
-      </button>
+//   return (
+//     <header className="header">
+//       <div className="logo">
+//         <img src="logo.png" alt="Everything I've Learned logo" />
+//         <h1>{appTitle}</h1>
+//       </div>
+//       <button className="btn btn-large btn-open" onClick={handleFormToggle}>
+//         {showForm ? "Close" : "Share your inspiration"}
+//       </button>
 
-      {showForm && (
-        <>
-          <div className="form-toggle"></div>
-          {showForm && (
-            <form className={`add-board-form ${showForm ? "show" : ""}`} onSubmit={handleBoardSubmit}>
-              <input
-                type="text"
-                placeholder="Add a new board..."
-                value={board}
-                onChange={(event) => setBoard(event.target.value)}
-              />
-              <button type="submit">Add Board</button>
-            </form>
-          )}
-        </>
-      )}
-    </header>
-  );
-}
+//       {showForm && (
+//         <>
+//           <div className="form-toggle"></div>
+//           {showForm && (
+//             <form className={`add-board-form ${showForm ? "show" : ""}`} onSubmit={handleBoardSubmit}>
+//               <input
+//                 type="text"
+//                 placeholder="Add a new board..."
+//                 value={board}
+//                 onChange={(event) => setBoard(event.target.value)}
+//               />
+//               <button type="submit">Add Board</button>
+//             </form>
+//           )}
+//         </>
+//       )}
+//     </header>
+//   );
+// }
 
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
---
+++ src/components/header.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+
+
+
+
+function Header({ showForm, setShowForm, addBoard, handleFormToggle, handleNewBoardToggle, handleExistingBoardToggle }) {
+    const appTitle = "Everything I've Learned, I Got From...";
+    const [board, setBoard] = useState("");
+
+    const handleBoardSubmit = (event) => {
+        event.preventDefault();
+    
+        if (board) {
+            const newBoard = { name: board, color: getRandomColor() };
+            addBoard(newBoard);
+            setBoard("");
+        }
+        };
+    
+        return (
+        <header className="header">
+            <div className="logo">
+            <img src="logo.png" alt="Everything I've Learned logo" />
+            <h1>{appTitle}</h1>
+            </div>
+            <button className="btn btn-large btn-open" onClick={handleFormToggle}>
+            {showForm ? "Close" : "Share your inspiration"}
+            </button>
+    
+            {showForm && (
+            <>
+                <div className="form-toggle"></div>
+                {showForm && (
+                <form className={`add-board-form ${showForm ? "show" : ""}`} onSubmit={handleBoardSubmit}>
+                    <input
+                    type="text"
+                    placeholder="Add a new board..."
+                    value={board}
+                    onChange={(event) => setBoard(event.target.value)}
+                    />
+                    <button type="submit">Add Board</button>
+                </form>
+                )}
+            </>
+            )}
+        </header>
+        );
+    }
+
+export default Header;
